4fbb242e5a9f68ffb90179fb19511bb0d21e9c8d
Ubin Jung <binimutter@gmail.com>
2023-03-20 10:39:23 -0500

    first java program

---
+++ assignments/java/FirstJavaProgram/FirstJava.java
@@ -0,0 +1,9 @@
+package assignments.java.FirstJavaProgram;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Ubin Jung");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Flower Mound, TX");
+    }
+}
---
+++ notes/java/day1/DayOne.java
@@ -0,0 +1,7 @@
+package day1;
+public class DayOne {
+    public static void main(String[] args) {
+        System.out.println("Whats Up class");
+        System.out.println("Whats Upp class");
+    }
+}
